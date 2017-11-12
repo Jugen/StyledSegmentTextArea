59124437e2d2cb7ba19017ec433d090ac0afb122
Jurgen <dollj@xsinet.co.za>
2017-11-12 14:36:03 +0200

    Update LabelSegment.java
    Updated to accommodate RichTextFX separation of segment and style.

--- lightware/richtext/LabelSegment.java
+++ lightware/richtext/LabelSegment.java
@@ -4,17 +4,17 @@ import javafx.scene.control.Label;
 
 public class LabelSegment extends AbstractSegment
 {
-	public LabelSegment( Object data, String style )
+	public LabelSegment( Object data )
 	{
-		super( data, style );
+		super( data );
 	}
 
 	@Override
-	public Node createNode()
+	public Node createNode( String style )
 	{
     	Label  item = new Label( data.toString() );
     	item.setStyle( "-fx-border-width: 1; -fx-border-style: solid; -fx-border-color: lightgrey; -fx-padding: 0 2 0 2; -fx-font-weight: normal; -fx-font-size: 10px;" );
-		if ( style != NO_STYLE ) item.getStyleClass().add( style );
+		if ( style != null &&  ! style.isEmpty() ) item.getStyleClass().add( style );
 		item.setUserData( getData() );
     	return item;
 	}
